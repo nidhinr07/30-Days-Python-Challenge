a419e040bc171839bbb92d4d591a1189c2ce74fa
nidhinr07 <nidhinjunior117@gmail.com>
2026-07-30 22:06:16 +0530

    Day 01: Added Python introduction and basic print concepts

---
+++ Day01-introduction/day01_introduction.py
@@ -0,0 +1,40 @@
+# Day 01 - Python Introduction
+
+# Single Line Comment
+# This program demonstrates basic Python concepts
+
+
+# Basic Print Statement
+print("Hello Python")
+
+
+# Multi-Line Comment
+"""
+This is a multi-line comment.
+Python ignores this while executing the program.
+"""
+
+
+# Escape Sequence - New Line
+print("Hello\nWelcome to Python")
+
+
+# sep Parameter
+print("Python", "Java", "C++", sep=" - ")
+
+
+# end Parameter
+print("Learning", end=" ")
+print("Python")
+
+
+# Getting Input From User
+name = input("Enter your name: ")
+
+print("Welcome", name)
+
+
+# Taking Integer Input
+age = int(input("Enter your age: "))
+
+print("Your age is", age)
