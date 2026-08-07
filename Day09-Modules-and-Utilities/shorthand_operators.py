5c9347a097b8933b53c64bab17a7fbde55f4fe89
Nidhin R <nidhinjunior117@gmail.com>
2026-08-07 22:49:26 +0530

    Add Day09-shorthand if-else
    Demonstrate various shorthand operators in Python.

---
+++ Day09-Modules-and-Utilities/shorthand_operators.py
@@ -0,0 +1,77 @@
+# Day 09 - Shorthand Operators
+
+# -----------------------------------
+# += Operator
+# -----------------------------------
+
+number = 10                      # Store value
+
+number += 5                      # Add and assign
+
+print(number)
+
+
+# -----------------------------------
+# -= Operator
+# -----------------------------------
+
+number = 20                      # Store value
+
+number -= 5                      # Subtract and assign
+
+print(number)
+
+
+# -----------------------------------
+# *= Operator
+# -----------------------------------
+
+number = 10                      # Store value
+
+number *= 3                      # Multiply and assign
+
+print(number)
+
+
+# -----------------------------------
+# /= Operator
+# -----------------------------------
+
+number = 20                      # Store value
+
+number /= 4                      # Divide and assign
+
+print(number)
+
+
+# -----------------------------------
+# //= Operator
+# -----------------------------------
+
+number = 20                      # Store value
+
+number //= 3                     # Floor divide
+
+print(number)
+
+
+# -----------------------------------
+# %= Operator
+# -----------------------------------
+
+number = 20                      # Store value
+
+number %= 3                      # Find remainder
+
+print(number)
+
+
+# -----------------------------------
+# **= Operator
+# -----------------------------------
+
+number = 5                       # Store value
+
+number **= 2                     # Find power
+
+print(number)
